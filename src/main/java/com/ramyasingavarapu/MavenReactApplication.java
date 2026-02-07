7782f2a3fb807716b3dca68698414be5bddc03ae
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2026-02-07 10:25:19 +0100

    Initialize project from maven-react template

---
+++ src/main/java/com/ramyasingavarapu/MavenReactApplication.java
@@ -0,0 +1,11 @@
+package com.ramyasingavarapu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionBoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActionBoardApplication.class, args);
+    }
+}
